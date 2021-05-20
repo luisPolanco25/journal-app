25e316be21183241756dad01e1fc1b114182d1dc
LuisP <luisjavierpolancomorel@gmail.com>
2021-05-19 21:44:30 -0400

    Updates

---
+++ precache-manifest.2fd06aac989b60b164f1d790d9b1f3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2102048bc0bbf3dc9311d6a6a440055",
+    "url": "/journal-app/index.html"
+  },
+  {
+    "revision": "0cca575a2ec6ef9a663c",
+    "url": "/journal-app/static/css/main.22f2f422.chunk.css"
+  },
+  {
+    "revision": "73d89faef4eca138a256",
+    "url": "/journal-app/static/js/2.a56889e6.chunk.js"
+  },
+  {
+    "revision": "9dfbab745ddad106d73206c378a68235",
+    "url": "/journal-app/static/js/2.a56889e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cca575a2ec6ef9a663c",
+    "url": "/journal-app/static/js/main.d8d830da.chunk.js"
+  },
+  {
+    "revision": "7369199aa78be9551bdd",
+    "url": "/journal-app/static/js/runtime-main.01427f0e.js"
+  }
+]);
